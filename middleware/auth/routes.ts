601aa1543096a3109e9b9b3328415cb178e4c407
Diego <diegosferas@gmail.com>
2020-09-06 21:38:35 -0500

    sd

--- middleware/auth/routes.ts
+++ middleware/auth/routes.ts
@@ -1,7 +1,7 @@
 import { Context } from "https://deno.land/x/oak@v6.0.2/context.ts";
 import RESOLVER from "../auth/resolvers.ts";
 
-export const login = async (ctx: Context) => {
+/* export const login = async (ctx: Context) => {
   const value = await ctx.request.body().value;
   const succesfull = await RESOLVER.login(value);
 
@@ -15,6 +15,30 @@ export const login = async (ctx: Context) => {
     ctx.response.body = {
       token: succesfull.token,
     };
+  }
+}; */
+export const login = async (ctx: Context) => {
+  const value = await ctx.request.body().value;
+  //const succesfull = await RESOLVER.login(value);
+  console.log(value);
+  const succesfull = {
+    error: {
+      type: "Autentification Failed",
+      detail: "User doesn't exist!",
+      status: 404,
+    },
+  };
+
+  if (succesfull.error) {
+    ctx.response.status = succesfull.error.status;
+    ctx.response.body = {
+      ...succesfull,
+    };
+  } else {
+    ctx.response.status = 200;
+    /*       ctx.response.body = {
+      token: succesfull.token,
+    }; */
   }
 };
 
